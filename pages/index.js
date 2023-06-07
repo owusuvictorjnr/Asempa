3b5d0010437a7b0d3cd116c3e27ea914330a32b2
owusuvictorjnr <owusuvictor80@gmail.com>
2023-06-07 08:19:03 -0400

    Wrap layout around home page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,5 @@
-import Head from 'next/head'
+import Layout from '@/components/Layout'
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Home</title>
-        <meta name="description" content="" />
-      </Head>
-
-      <h1 className="text-3xl font-bold capitalize">asempa brand </h1>
-    </div>
-  )
+  return <Layout title="Home Page">Home Page</Layout>
 }
