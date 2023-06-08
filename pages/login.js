b422b68db7e5d82c087c0c0e399560714d27ba62
owusuvictorjnr <owusuvictor80@gmail.com>
2023-06-08 14:51:27 -0400

    create login page and validate it

---
+++ pages/login.js
@@ -0,0 +1,75 @@
+import Layout from '@/components/Layout'
+import Link from 'next/link'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+export default function LoginPage() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm()
+
+  const submitHandler = ({ email, password }) => {
+    console.log(email, password)
+  }
+
+  return (
+    <Layout title="Login">
+      <form
+        className="mx-auto max-w-screen-md"
+        onSubmit={handleSubmit(submitHandler)}
+      >
+        <h1 className="mb-4 text-xl">Login</h1>
+        <div className="mb-4">
+          <label htmlFor="email">Email</label>
+          <input
+            {...register('email', {
+              required: 'Please enter an email',
+              pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/,
+                message: 'Please enter a valid email',
+              },
+            })}
+            type="email"
+            className="w-full"
+            id="email"
+            autofocus
+          />
+          {errors.email && (
+            <div className="text-red-500">{errors.email.message}</div>
+          )}
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="email">Password</label>
+          <input
+            {...register('password', {
+              required: 'Please enter your password',
+              minLength: {
+                value: 8,
+                message: 'Password must be at least 8 characters',
+              },
+            })}
+            type="password"
+            className="w-full"
+            id="password"
+            autofocus
+          />
+          {errors.password && (
+            <div className="text-red-500">{errors.password.message}</div>
+          )}
+        </div>
+
+        <div className="mb-4">
+          <button className="primary-btn">Login</button>
+        </div>
+
+        <div className="mb-4">
+          Don&apos;t have an account? &nbsp;
+          <Link href="register">Register</Link>
+        </div>
+      </form>
+    </Layout>
+  )
+}
