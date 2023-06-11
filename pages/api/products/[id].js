56c90653208f381e4f52fe1c3422d7fb7b504ff2
owusuvictorjnr <owusuvictor80@gmail.com>
2023-06-11 10:10:01 -0400

    api to get product by id

---
+++ pages/api/products/[id].js
@@ -0,0 +1,12 @@
+import db from '@/utils/db'
+
+const { default: Product } = require('@/models/Product')
+
+const handler = async (req, res) => {
+  await db.connect()
+  const product = await Product.findById(req.query.id)
+  await db.disconnect()
+  res.send(product)
+}
+
+export default handler
