4887074f13f14e548ae8f2d87464e6bdf5dbd5c5
owusuvictorjnr <owusuvictor80@gmail.com>
2023-07-10 03:54:41 -0400

    error corrected

--- models/Order.js
+++ models/Order.js
@@ -35,5 +35,5 @@ const orderSchema = new mongoose.Schema(
   }
 )
 
-const Order = mongoose.models.Order || mongoose.models('Order', orderSchema)
+const Order = mongoose.models.Order || mongoose.model('Order', orderSchema)
 export default Order
