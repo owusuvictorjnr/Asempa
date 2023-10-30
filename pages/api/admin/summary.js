f5ab578a983cc10b8f826b07e7d54fd7efea4378
owusuvictorjnr <owusuvictor80@gmail.com>
2023-10-30 20:33:54 +0000

    sales not sale

--- pages/api/admin/summary.js
+++ pages/api/admin/summary.js
@@ -20,7 +20,7 @@ const handler = async (req, res) => {
     {
       $group: {
         _id: null,
-        sale: { $sum: '$totalPrice' },
+        sales: { $sum: '$totalPrice' },
       },
     },
   ])
