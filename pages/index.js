33a6cd40ebde8ddff11a1d94286ed519cde4ca21
owusuvictorjnr <owusuvictor80@gmail.com>
2023-11-01 08:12:52 +0000

    import carusel and implement isFeatured

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,11 @@ import db from '@/utils/db'
 import axios from 'axios'
 import { useContext } from 'react'
 import { toast } from 'react-toastify'
+import { Carousel } from 'react-responsive-carousel'
+import 'react-responsive-carousel/lib/styles/carousel.min.css'
+import Link from 'next/link'
 
-export default function Home({ products }) {
+export default function Home({ products, featuredProducts }) {
   const { state, dispatch } = useContext(Store)
   const { cart } = state
 
@@ -27,6 +30,17 @@ export default function Home({ products }) {
 
   return (
     <Layout title="Home Page">
+      <Carousel showThumbs={false} autoPlay={true} infiniteLoop>
+        {featuredProducts.map((product) => (
+          <div key={product._id}>
+            <Link href={`/product/${product.slug}`} passHref className="flex">
+              <img src={product.banner} alt={product.name} />
+            </Link>
+          </div>
+        ))}
+      </Carousel>
+
+      <h2 className="capitalize h-2 my-4">latest products</h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
         {products.map((product) => (
           <ProductItem
@@ -43,8 +57,11 @@ export default function Home({ products }) {
 export async function getServerSideProps() {
   await db.connect()
   const products = await Product.find().lean()
+  const featuredProducts = await Product.find({ isFeatured: true }).lean()
+
   return {
     props: {
+      featuredProducts: featuredProducts.map(db.convertDocToObj),
       products: products.map(db.convertDocToObj),
     },
   }
